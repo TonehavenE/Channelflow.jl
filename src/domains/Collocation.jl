f4634dfdcd377845e88be38f876b5cbc2f1effdc
Eben Quenneville <tonehaven@outlook.com>
2025-08-03 23:07:25 -0400

    add: wrapper over Chebyshev and Fourier grids

---
+++ src/domains/Collocation.jl
@@ -0,0 +1,13 @@
+module Collocation
+
+include("./grid.jl")
+include("./chebyshev.jl")
+include("./fourier.jl")
+
+using .Chebyshev
+using .Grid
+using .Fourier
+
+export ChebyshevGrid, FourierGrid, fourier_derivative_matrix, chebyshev_derivative_matrix, AbstractGrid
+
+end
