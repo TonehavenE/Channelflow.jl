29357855cdc960732d96da226ca4639132a1c866
Eben Quenneville <tonehaven@outlook.com>
2025-08-03 23:09:06 -0400

    add: combining everything in central files

--- src/Channelflow.jl
+++ src/Channelflow.jl
@@ -1,3 +1,16 @@
 module Channelflow
 
+# Write your package code here.
+include("domains/Collocation.jl")
+include("DifferentialOperators.jl")
+include("HelmholtzSolver.jl")
+include("TimeSteppers.jl")
+
+using .Collocation
+using .DifferentialOperators
+using .HelmholtzSolver
+using .TimeSteppers
+
+export ChebyshevGrid, FourierGrid, fourier_derivative_matrix, differentiation_operator, differentiation_operators, solve_helmholtz, timestep_CNAB2, SimulationParameters
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
-using ChannelFlow
+using Channelflow
 using Test
 
 @testset "ChannelFlow.jl" begin
     # Write your tests here.
+    include("chebyshev_tests.jl")
+    include("fourier_tests.jl")
+    include("differentiation_operator_tests.jl")
+    include("helmholtz_tests.jl")
 end
