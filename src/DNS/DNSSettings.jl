31b769bad56b1f5c95d7e996a04f4f5bb339ca97
Eben Quenneville <tonehaven@outlook.com>
2025-08-15 16:23:35 -0400

    docs: DNSSettings meaning

--- src/DNS/DNSSettings.jl
+++ src/DNS/DNSSettings.jl
@@ -1,5 +1,49 @@
 module DNSSettings
 
+export VelocityScale,
+    WallScale,
+    ParabolicScale,
+    ZeroBase,
+    LinearBase,
+    ParabolicBase,
+    LaminarBase,
+    SuctionBase,
+    ArbitraryBase,
+    PressureGradient,
+    BulkVelocity,
+    TimeStepMethod,
+    CNFE1,
+    CNAB2,
+    CNRK2,
+    SMRK2,
+    SBDF1,
+    SBDF2,
+    SBDF3,
+    SBDF4,
+    NonlinearMethod,
+    Rotational,
+    Divergence,
+    SkewSymmetric,
+    Alternating,
+    LinearAboutProfile,
+    Dealiasing,
+    NoDealiasing,
+    DealiasXZ,
+    DealiasY,
+    DealiasXYZ,
+    Verbosity,
+    Silent,
+    PrintTime,
+    PrintTicks,
+    VerifyTauSolve,
+    PrintAll,
+    DNSFlags,
+    TimeStep,
+    adjust!,
+    adjust_to_middle!,
+    adjust_to_desired!,
+    adjust_for_T!
+
 @enum VelocityScale WallScale ParabolicScale
 @BaseFlow ZeroBase LinearBase ParabolicBase LaminarBase SuctionBase ArbitraryBase
 @MeanConstraint PressureGradient BulkVelocity
@@ -8,6 +52,45 @@ module DNSSettings
 @enum Dealiasing NoDealiasing DealiasXZ DealiasY DealiasXYZ
 @enum Verbosity Silent PrintTime PrintTicks VerifyTauSolve PrintAll
 
+"""
+    DNSFlags
+
+Defines the settings and configuration flags for a DNS simulation.
+The parameters are as follows:
+- nu: Kinematic viscosity
+- dPdx: Constraint value for the x-direction mean flow pressure gradient
+- dPdz: Constraint value for the z-direction mean flow pressure gradient
+- Ubulk: Constraint value for the mean flow: bulk velocity in x
+- Wbulk: Constraint value for the mean flow: bulk velocity in z
+- Uwall: wall speed downstream
+- ulowerwall: lower wall speed along x, e.g. -1 for plane couette
+- uupperwall: upper wall speed along x, e.g. 1 for plane couette
+- wlowerwall: lower wall speed along z
+- wupperwall: upper wall speed along z
+- theta: tilt of the domain relative to downstream
+- Vsuck: suction velocity
+- rotation: dimensionless rotation around the z-axis
+- t0: start time 
+- T: final time 
+- dT: print interval
+- dt: time step interval 
+- variabledt: whether to use variable time stepping
+- dtmin: lower bound for time step
+- dtmax: upper bound for time step
+- CFLmin: lower bound for CFL number
+- CFLmax: upper bound for CFL number
+- symmetryprojectioninterval: only project onto symmetries at this interval
+- baseflow: base flow profile, utot = u + Ubase(y) e_x
+- constraint: mean flow constraint type (constant pressure gradient or constrant bulk velocity)
+- timestepping: time stepping method
+- initstepping: initial time stepping method
+- nonlinearity: nonlinearity method
+- dealiasing: dealiasing method
+- bodyforce: body force term
+- taucorrection: whether to apply tau correction (the divergence caused by discretization)
+- verbosity: whether to print diagnostics, times, ticks, or nothing
+- logstream: stream to log output
+"""
 struct DNSFlags
     nu::Real
     dPdx::Real
@@ -38,28 +121,82 @@ struct DNSFlags
     initstepping::TimeStepMethod
     nonlinearity::NonlinearMethod
     dealiasing::Dealiasing
-    bodyforce
+    bodyforce::Any
     taucorrection::Bool
     verbosity::Verbosity
-    logstream
+    logstream::Any
 end
 
 function DNSFlags(;
-    nu=0.0025, dPdx=0.0, dPdz=0.0, Ubulk=0.0, Wbulk=0.0, Uwall=1.0,
-    ulowerwall=0.0, uupperwall=0.0, wlowerwall=0.0, wupperwall=0.0,
-    theta=0.0, Vsuck=0.0, rotation=0.0, t0=0.0, T=20.0, dT=1.0,
-    dt=0.03125, variabledt=true, dtmin=0.001, dtmax=0.2, CFLmin=0.4,
-    CFLmax=0.6, symmetryprojectioninterval=100.0,
-    baseflow=:LaminarBase, constraint=:PressureGradient, timestepping=:SBDF3,
-    initstepping=:SMRK2, nonlinearity=:Rotational, dealiasing=:DealiasXZ,
-    bodyforce=nothing, taucorrection=true, verbosity=:PrintTicks, logstream=stdout
+    nu = 0.0025,
+    dPdx = 0.0,
+    dPdz = 0.0,
+    Ubulk = 0.0,
+    Wbulk = 0.0,
+    Uwall = 1.0,
+    ulowerwall = 0.0,
+    uupperwall = 0.0,
+    wlowerwall = 0.0,
+    wupperwall = 0.0,
+    theta = 0.0,
+    Vsuck = 0.0,
+    rotation = 0.0,
+    t0 = 0.0,
+    T = 20.0,
+    dT = 1.0,
+    dt = 0.03125,
+    variabledt = true,
+    dtmin = 0.001,
+    dtmax = 0.2,
+    CFLmin = 0.4,
+    CFLmax = 0.6,
+    symmetryprojectioninterval = 100.0,
+    baseflow = :LaminarBase,
+    constraint = :PressureGradient,
+    timestepping = :SBDF3,
+    initstepping = :SMRK2,
+    nonlinearity = :Rotational,
+    dealiasing = :DealiasXZ,
+    bodyforce = nothing,
+    taucorrection = true,
+    verbosity = :PrintTicks,
+    logstream = stdout,
 )
-    return DNSFlags(nu, dPdx, dPdz, Ubulk, Wbulk, Uwall,
-        ulowerwall, uupperwall, wlowerwall, wupperwall,
-        theta, Vsuck, rotation, t0, T, dT, dt, variabledt, dtmin, dtmax,
-        CFLmin, CFLmax, symmetryprojectioninterval, baseflow, constraint,
-        timestepping, initstepping, nonlinearity, dealiasing, bodyforce,
-        taucorrection, verbosity, logstream)
+    return DNSFlags(
+        nu,
+        dPdx,
+        dPdz,
+        Ubulk,
+        Wbulk,
+        Uwall,
+        ulowerwall,
+        uupperwall,
+        wlowerwall,
+        wupperwall,
+        theta,
+        Vsuck,
+        rotation,
+        t0,
+        T,
+        dT,
+        dt,
+        variabledt,
+        dtmin,
+        dtmax,
+        CFLmin,
+        CFLmax,
+        symmetryprojectioninterval,
+        baseflow,
+        constraint,
+        timestepping,
+        initstepping,
+        nonlinearity,
+        dealiasing,
+        bodyforce,
+        taucorrection,
+        verbosity,
+        logstream,
+    )
 end
 
 struct TimeStep
@@ -74,9 +211,15 @@ struct TimeStep
     variable::Bool
 end
 
-function TimeStep(dt::Real, dt_min::Real,
-    dt_max::Real, dT::Real, CFL_min::Real, CFL_max::Real,
-    variable::Bool)
+function TimeStep(
+    dt::Real,
+    dt_min::Real,
+    dt_max::Real,
+    dT::Real,
+    CFL_min::Real,
+    CFL_max::Real,
+    variable::Bool,
+)
     @assert dt_min > 0.0 "dt_min must be positive"
     @assert dt > 0.0 "dt must be positive"
     @assert dt > dt_min "dt must be greater than dt_min"
@@ -107,12 +250,20 @@ function TimeStep(dt::Real, dt_min::Real,
 end
 
 function TimeStep(flags::DNSFlags)
-    TimeStep(flags.dt, flags.dtmin, flags.dtmax, flags.dT, flags.CFLmin, flags.CFLmax, flags.variabledt)
+    TimeStep(
+        flags.dt,
+        flags.dtmin,
+        flags.dtmax,
+        flags.dT,
+        flags.CFLmin,
+        flags.CFLmax,
+        flags.variabledt,
+    )
 end
 
 function adjust!(ts::TimeStep, CFL::Real)
     ts.CFL = CFL
-    if ts.variable && (CFL <= ts.CFLmin || CFL >+ ts.CFL_max)
+    if ts.variable && (CFL <= ts.CFLmin || CFL > +ts.CFL_max)
         return adjust_to_middle!(ts, CFL)
     end
     return false
@@ -127,7 +278,7 @@ function iround(x::Real)
 end
 
 function adjust_to_middle!(ts::TimeStep, CFL::Real)
-    if (ts.dt_min == ts.dt_max) || ts.dT == 0.0 
+    if (ts.dt_min == ts.dt_max) || ts.dT == 0.0
         return false
     end
 
@@ -158,7 +309,7 @@ end
 
 function adjust_to_desired!(ts:TimeStep, a::Real, a_desired::Real)
     ai = a
-    if (ts.dt_min == ts.dt_max) || ts.dT == 0.0 
+    if (ts.dt_min == ts.dt_max) || ts.dT == 0.0
         return false
     end
 
@@ -207,7 +358,7 @@ function adjust_for_T!(ts::TimeStep, T::Real)
 
     N = max(iround(T / ts.dT), 1)
     dT = T / N
-    n = max(iround(dT/ts.dt), 1)
+    n = max(iround(dT / ts.dt), 1)
     dt = dT / n
 
     while dt < ts.dt_min && n >= 2 && dT != 0
@@ -225,7 +376,7 @@ function adjust_for_T!(ts::TimeStep, T::Real)
     adjustment = (dt == ts.dt) ? false : true
     ts.n = n
     ts.N = N
-    ts.dt = dt 
+    ts.dt = dt
     ts.dT = dT
     ts.CFL = CFL
     return adjustment
